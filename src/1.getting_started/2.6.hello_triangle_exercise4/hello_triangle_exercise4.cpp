7eb12d16f9d192bbeb39dd4e43534503ba4b8219
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-09-25 17:52:08 -0400

    Added starter files for hello_triangle_exercise4

---
+++ src/1.getting_started/2.6.hello_triangle_exercise4/hello_triangle_exercise4.cpp
@@ -0,0 +1,6 @@
+// Question: Create two shader programs where the second program uses a different fragment shader that outputs the color yellow; draw both triangles again where one outputs the color yellow and the other outputs the color orange. The yellow triangle should point to the left and the red triangle should point to the right.
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
