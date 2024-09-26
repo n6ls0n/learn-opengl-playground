392957219d4eab43db354bbfa64bba900ce04ab5
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-09-26 15:47:13 -0400

    Added build and Cmake files

---
+++ src/1.getting_started/3.1.shaders_uniform/build_run_project.bat
@@ -0,0 +1,32 @@
+@echo off
+
+REM Check if build directory exists, delete it if it does
+if exist build (
+    rmdir /s /q build
+)
+
+REM Create a new build directory if it doesn't exist
+if not exist build (
+    mkdir build
+)
+
+REM Change to the build directory
+cd build
+
+REM Run CMake to generate build files
+cmake ..
+
+REM Build the project
+cmake --build .
+
+REM Change to the Debug directory
+cd Debug
+
+REM Run the resulting executable (hello_triangle_exercise4.exe)
+shaders_uniform.exe
+
+REM Change back to the original directory
+cd ..
+
+REM Pause to keep the command window open
+pause
