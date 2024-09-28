5a0163d12a518ae2b57367344b34a10f4767d41b
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-09-28 09:05:39 -0400

    Removed comments

--- src/1.getting_started/3.5.shaders_exercise2/shaders_exercise2.cpp
+++ src/1.getting_started/3.5.shaders_exercise2/shaders_exercise2.cpp
@@ -1,26 +1,5 @@
 // Question Specify a horizontal offset via a uniform and move the triangle to the right side of the screen in the vertex shader using this offset value:
 
-// // In your CPP file:
-// // ======================
-// float offset = 0.5f;
-// ourShader.setFloat("xOffset", offset);
-
-// // In your vertex shader:
-// // ======================
-// #version 330 core
-// layout (location = 0) in vec3 aPos;
-// layout (location = 1) in vec3 aColor;
-
-// out vec3 ourColor;
-
-// uniform float xOffset;
-
-// void main()
-// {
-//     gl_Position = vec4(aPos.x + xOffset, aPos.y, aPos.z, 1.0); // add the xOffset to the x position of the vertex position
-//     ourColor = aColor;
-// }
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
