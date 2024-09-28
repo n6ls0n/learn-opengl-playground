aafaed6830715f3f75e3d494e26e2bf1a4fe1986
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-09-28 10:42:06 -0400

    Minor edit

--- src/1.getting_started/3.5.shaders_exercise2/shaders_exercise2.cpp
+++ src/1.getting_started/3.5.shaders_exercise2/shaders_exercise2.cpp
@@ -1,4 +1,4 @@
-// Question Specify a horizontal offset via a uniform and move the triangle to the right side of the screen in the vertex shader using this offset value:
+// Question: Specify a horizontal offset via a uniform and move the triangle to the right side of the screen in the vertex shader using this offset value:
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
