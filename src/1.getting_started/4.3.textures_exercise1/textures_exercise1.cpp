acd875435af7109e15d8fe8384ed0cce004a2f24
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-09-30 10:21:35 -0400

    Removed comments

--- src/1.getting_started/4.3.textures_exercise1/textures_exercise1.cpp
+++ src/1.getting_started/4.3.textures_exercise1/textures_exercise1.cpp
@@ -1,18 +1,4 @@
 // Question: Make sure only the happy face looks in the other/reverse direction by changing the fragment shader
-// #version 330 core
-// out vec4 FragColor;
-
-// in vec3 ourColor;
-// in vec2 TexCoord;
-
-// uniform sampler2D ourTexture1;
-// uniform sampler2D ourTexture2;
-
-// void main()
-// {
-//     FragColor = mix(texture(ourTexture1, TexCoord), texture(ourTexture2, vec2(1.0 - TexCoord.x, TexCoord.y)), 0.2);
-// }
-
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
