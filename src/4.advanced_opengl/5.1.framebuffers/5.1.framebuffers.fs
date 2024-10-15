a20a87bea97995b1e0d612b7d3aa5bd2fe263fa5
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-10-14 23:19:10 -0400

    Added grey-scale effect in 5.1.framebuffers

--- src/4.advanced_opengl/5.1.framebuffers/5.1.framebuffers.fs
+++ src/4.advanced_opengl/5.1.framebuffers/5.1.framebuffers.fs
@@ -11,5 +11,11 @@ void main()
 
     // Inversion effect
     // ---------------------------------------------------------------
-    FragColor = vec4(vec3(1.0 - texture(texture1, TexCoords)), 1.0);
+    // FragColor = vec4(vec3(1.0 - texture(texture1, TexCoords)), 1.0);
+
+    // Grey Scale Effect
+    // ---------------------------------------------------------------
+    FragColor = texture(texture1, TexCoords);
+    float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
+    FragColor = vec4(average, average, average, 1.0);
 }
